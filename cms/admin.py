5571195f3ce3426ab7c90c35a54699cea9f4694a
riku <gety0urpingdrum@gmail.com>
2016-02-05 01:47:21 +0900

    add admin.py
    Admin画面での情報量を増やした

--- cms/admin.py
+++ cms/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from cms.models import Subject, Attendance
 
+
 # Register your models here.
 
-admin.site.register(Subject)
-admin.site.register(Attendance)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'subject_name', 'period', 'day', 'absence')
+    list_display_links = ('id', 'subject_name')
+
+
+admin.site.register(Subject, SubjectAdmin)
+
+
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date', 'number_of_times', 'attendance')
+    list_display_links = ('id',)
+
+
+admin.site.register(Attendance, AttendanceAdmin)
