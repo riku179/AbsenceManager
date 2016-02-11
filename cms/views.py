5fc536937f8f138aa1bc27d113410e9932970ed6
riku <gety0urpingdrum@gmail.com>
2016-02-11 17:24:15 +0900

    add フォームからのrequestをDBに反映

--- cms/views.py
+++ cms/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 from django.http import HttpResponse
 from django.forms.models import modelformset_factory
 from cms.models import *
@@ -13,14 +14,14 @@ def index(request):
 
 def show_detail(request, subject_id):
     AttendanceFormSet = modelformset_factory(Attendance, extra=0, fields=('absence', ))
-    formset = AttendanceFormSet()
-    # Attendance.objects.filter(subject=subject_id).order_by('times'))
+    if request.method == 'POST':
+        formset = AttendanceFormSet(request.POST)
+        if formset.is_valid():
+            formset.save()
+    else:
+        formset = AttendanceFormSet()
     return render_to_response('cms/detail.html', {
         'subject': Subject.objects.get(id=subject_id),
         'attendances': Attendance.objects.filter(subject=subject_id).order_by('times'),
         'attend_formset': formset
-    })
-
-
-def update_attendance_status(request, subject_id):
-    pass
+    }, context_instance=RequestContext(request))
