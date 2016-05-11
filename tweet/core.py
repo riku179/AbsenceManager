08d57b812bcc9c441f52352b4ba768dce22beb91
riku <gety0urpingdrum@gmail.com>
2016-05-11 12:22:01 +0900

    add twitter連携機能・celeryによるタスク追加

--- tweet/core.py
+++ tweet/core.py
@@ -1,3 +1,37 @@
 from twitter import *
 from allauth.socialaccount.models import SocialToken
+from django.core.exceptions import ObjectDoesNotExist
 
+from tweet.tasks import *
+
+CONSUMER_KEY = csVH8LFOWjz4oIuhseDwnrY24
+CONSUMER_SECRET = Tx81hrPOGkAx1c8pyuIzPvTc8ZNFRL5nMbXGBjoeTmcnDMKS39
+
+def main():
+    token = SocialToken.objects.get(account__user=3).token
+    secret = SocialToken.objects.get(account__user=3).token_secret
+    auth = OAuth(token, secret, CONSUMER_KEY, CONSUMER_SECRET)
+    rest_api = Twitter(auth=auth)
+    streaming_api = TwitterStream(auth=auth, domain="userstream.twitter.com")
+
+    account_verify = rest_api.account.verify_credentials(skip_status='true')
+    screen_name = account_verify['screen_name']
+    user_id = account_verify['id'] #  アカウントの情報を取得する。skip_statusは最新のpostを引っ張ってくるのを無効化
+
+    followers_list = rest_api.followers.ids(screen_name=screen_name) # followerリスト
+
+    for msg in streaming_api.user():
+        if 'friends' in msg: # 接続が確立された時最初に1度のみ受け取る
+            pass
+
+        if 'event' in msg:
+            if msg['event'] == 'follow' and msg['target'] == user_id: # フォローされた
+                if followed_by_someone.delay(msg['source']):
+                    pass
+
+            if msg['event'] == 'unfollow' and msg['target'] == user_id: # リムーブされた
+                if removed_by_someone.delay(msg['source']):
+                    pass
+
+        if 'user' in msg:
+            pass
--- tweet/tasks.py
+++ tweet/tasks.py
@@ -1,11 +1,27 @@
 from celery import shared_task
 from twitter import *
+from allauth.socialaccount.models import SocialAccount
+from django.core.exceptions import ObjectDoesNotExist
+from authentication.models import UserProfile
 
 
 @shared_task
-def ready_get_messages():
-    pass
+def followed_by_someone(id):
+    try:
+        source_user = SocialAccount.objects.get(uid=id)
+    except ObjectDoesNotExist:
+        pass
+    source_user_profile = UserProfile.objects.get(user=source_user)
+    source_user_profile.watch_tl = True
+    source_user_profile.save()
+
 
 @shared_task
-def watch_tl():
-    pass
+def removed_by_someone(id):
+    try:
+        source_user = SocialAccount.objects.get(uid=id)
+    except ObjectDoesNotExist:
+        pass
+    source_user_profile = UserProfile.objects.get(user=source_user)
+    source_user_profile.watch_tl = False
+    source_user_profile.save
