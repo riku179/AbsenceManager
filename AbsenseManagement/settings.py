95cd40fca8c393c6a8701ef27e4b7d0381bebeba
archie <l19818128@gmail.com>
2016-11-11 20:21:09 +0900

    Remove celery files

--- AbsenseManagement/settings.py
+++ AbsenseManagement/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap3',
-    'celery_config',
     'table',
     'authentication',
     'tweet',
--- celery_config/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class CeleryConfig(AppConfig):
-    name = 'celery_config'
-
-    def ready(self):
-        from AbsenseManagement.celery_app import get_celery_app
-        get_celery_app()
